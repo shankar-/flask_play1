092fa26b352cedb5d68dafdc822af70d5d75ba29
shankar- <shankarbhatm@gmail.com>
2016-08-30 22:58:58 -0500

    Added 2nd route '/welcome' to the app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
+
+@app.route('/welcome')
+def welcome():
+    return render_template("welcome.html")
+
 
 if __name__ == '__main__':
     app.run()
